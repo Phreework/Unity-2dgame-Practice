5c5c84bd9a7108196d015c57d2664d6fa9b8b602
Phreework <phreework@gmail.com>
2018-04-06 11:45:39 +0800

    finish three scene for test
    -"slide muilt sprite"scene shows how to slide a tilespr or sheetspr in Unity and hoe get several spr together.
    -"sorting layer"scene shows how to sort sprite in game by sorting layer.the script "SortingLayer.cs"can explain detail.What important is new sorting layer is upper and bigger layerorder is upper.
    -"two way to create 2dAni"scene shows how to create animation in Unity.You can just select all frame and drag to scene or use a script to control a spriteObj change sprite.Obviously,the
     latter is more flexible,i love it,and i wirte a simple Script named "SpriteAnimator.cs".

---
+++ Unity 2dgame Practice/Assets/Scripts/SortingLayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingLayer : MonoBehaviour {
+    public int sortOrder;
+    public string sortLayerName;
+    private void Start() {
+        SetLayer();
+    }
+
+    //use to set 2dLayer ,bigger sortingOrder ,draw upper  
+    void SetLayer() {
+        SpriteRenderer r = this.GetComponent<SpriteRenderer>();
+        r.sortingLayerName = sortLayerName;
+        r.sortingOrder = sortOrder;
+    }
+ 
+}
---
+++ Unity 2dgame Practice/Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour {
+
+    //sprite renderer
+    protected SpriteRenderer m_sprite;
+    //sprite clips
+    public Sprite[] m_clips;
+    //ani timer
+    protected float m_aniTimer = 0.1f;
+    //frame
+    protected int m_frame = 0;
+
+	void Start () {
+        //add sprite renderer
+        m_sprite = this.gameObject.GetComponent<SpriteRenderer>();
+        //set first frame's sprite
+        m_sprite.sprite = m_clips[m_frame];
+	}
+	
+
+	void Update () {
+        m_aniTimer -= Time.deltaTime;
+
+        if (m_aniTimer <= 0) {
+            m_aniTimer = 0.1f;
+            m_frame++;
+            if (m_frame >= m_clips.Length)
+                m_frame = 0;
+            //renew
+            m_sprite.sprite = m_clips[m_frame];
+        }
+	}
+}
