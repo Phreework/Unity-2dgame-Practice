bc3d0f08ace3383cc50710caca1724d568e960cf
Phreework <781772991@qq.com>
2018-04-08 09:32:17 +0800

    fish game is nearly finished!
    had a little work only

---
+++ Unity 2dgame Practice/Assets/Scripts/fish game/Cannon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    float m_shootTimer = 0;
+	
+	void Update () {
+        UpdateInput();	
+	}
+
+    void UpdateInput() {
+        m_shootTimer -= Time.deltaTime;
+
+        Vector3 ms = Input.mousePosition;
+        ms = Camera.main.ScreenToWorldPoint(ms);
+
+        Vector3 mypos = this.transform.position;
+
+        if (Input.GetMouseButton(0)) {
+            Vector2 targetDir = ms - mypos;
+            float angle = Vector2.Angle(targetDir, Vector3.up);
+            if (ms.x > mypos.x)
+                angle = -angle;
+            this.transform.eulerAngles = new Vector3(0, 0, angle);
+
+            if(m_shootTimer <= 0) {
+                m_shootTimer = 0.1f;
+            }
+        }
+    }
+}
---
+++ Unity 2dgame Practice/Assets/Scripts/fish game/FishSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawn : MonoBehaviour {
+
+    public float timer = 0;
+    public int max_fish = 30;
+    public int fish_count = 0;
+
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            timer = 2.0f;
+            if (fish_count >= max_fish)
+                return;
+            int index = 1 + (int)(Random.value * 3.0f);
+            if (index > 3)
+                index = 3;
+            fish_count++;
+            GameObject fishprefab = (GameObject)Resources.Load("fish" + index);
+            float cameraz = Camera.main.transform.position.z;
+
+            Vector3 randpos = new Vector3(Random.value, Random.value, -cameraz);
+            randpos = Camera.main.ViewportToWorldPoint(randpos);
+
+            Fish.Dir dir = Random.value > 0.5f ? Fish.Dir.Right : Fish.Dir.Left;
+            Fish f = Fish.Create(fishprefab, dir, randpos);
+
+            f.OnDeath += OnDeath;
+        }
+	}
+    void OnDeath(Fish fish) {
+        fish_count--;
+    }
+}
