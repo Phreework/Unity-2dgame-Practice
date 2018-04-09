1da7803e9b2323ba809d5baae1f317da880865bf
Phreework <phreework@gmail.com>
2018-04-09 19:26:08 +0800

    Add last test
    -Add a test about change 2Dmaterial,though it didnt show in the Inspector

---
+++ Unity 2dgame Practice/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer render = this.GetComponent<SpriteRenderer>();
+        render.material.color = new Color(1, 0, 0, 1);
+	}
+	
+}
